fadd4191140309bc07d05bbff259801e974c5376
sajoh002 <sajoh002@gmail.com>
2021-09-23 13:17:48 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>
-        <a
-          href="https://github.com/sajoh002/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Coded
-        </a>{" "}
-        by Sarah Johnson
-      </p>
+      <div className="container">
+        <p>
+          <a
+            href="https://github.com/sajoh002/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Coded
+          </a>{" "}
+          by Sarah Johnson
+        </p>
+      </div>
     </div>
   );
 }
